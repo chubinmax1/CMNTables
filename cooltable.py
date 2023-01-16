7a9f0f2f9a443855dad9e9a4c2caeee5e8a75227
mchubin <mchubin@arti.ru>
2023-01-16 23:10:20 +0300

    Исправлены ошибки. Добавлено установка цвета фона заголовка. Добавлены 13 и 14 стили. Добавлено выделение текста заголовка жирным.

--- cooltable.py
+++ cooltable.py
@@ -2,58 +2,28 @@
 # -*- coding: utf-8 -*-
 """
 @Author: Max N Chubin <chubinmax@yandex.ru>
-@Date: 2022-04-14
+@Date: 2023-01-16
 """
 class Mtable:
 	'''
 	Max Chubin <chubinmax@yandex.r>
 	MaxTable - data output in tabular form.
-	Many styles of borders, different colors for borders, title, header, data, intends for best look
+	Many styles of borders, different colors for borders, title, header, data, intends and shadow for best look
 	'''
-	# символы для отрисовки узлов таблицы
-	styles = [	['┌','─','┐','└','┘','│','┬','┴','┼','├','┤','│','─'],	# 0
- 				['╔','═','╗','╚','╝','║','╦','╩','╬','╠','╣','║','═'],	# 1
-				['╔','═','╗','╚','╝','║','╤','╧','┼','╟','╢','│','─'],	# 2
-				['┌',' ','┐','└','┘',' ',' ',' ','┼',' ',' ','│','─'],	# 3
-				['┌','─','┐','└','┘','│','┬','┴',' ','├','┤',' ',' '],	# 4
-				['┌','─','┐','└','┘','│','─','─',' ','│','│',' ',' '],	# 5
-				['╔','═','╗','╚','╝','║','╤','╧',' ','╟','╢',' ',' '],	# 6
-				['╔','═','╗','╚','╝','║','═','═',' ','║','║',' ',' '],	# 7
-				['+','-','+','+','+','|','-','-','+','|','|','|','-'],	# 8
-				['+','=','+','+','+','|','=','=','+','|','|','|','='],	# 9
-				['┌',' ','┐','└','┘',' ','┬','┴','┼','├','┤',' ',' '],	# 10
-				['┌',' ','┐','└','┘',' ',' ',' ',' ',' ',' ',' ',' '],	# 11
-				[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']	# 12
-			]
-	text_colors = {
-		'black': 	30,
-		'red': 		31,
-		'green': 	32,
-		'yellow':	33,
-		'blue': 	34,
-		'violet': 	35,
-		'turquoise': 36,
-		'white': 	37,
-	}
-	def color(self, text, color = 'white', bold = False, cont = False):
-		if self.tofile:
-			out = text
-		else:
-			col = self.text_colors.get(color)
-			BOLD = ""
-			if bold:
-				BOLD = "\033[1m"
-			if cont:
-				fin = ""
-			else:
-				fin = "\033[0m"
-			out = f"{BOLD}\033[{col}m{text}{fin}"
-		return out
+	COLOR_BLACK = 30
+	COLOR_RED = 31
+	COLOR_GREEN = 32
+	COLOR_YELLOW = 33
+	COLOR_BLUE = 34
+	COLOR_VIOLET = 35
+	COLOR_TURQUOISE = 36
+	COLOR_WHITE = 37
 
-	# символы для отрисовки тени
-	shadow_symbol = ['▐','▀']
+	ALIGMENT_LEFT = 'left'
+	ALIGMENT_CENTER = 'center'
+	ALIGMENT_RIGHT = 'right'
 
-	def __init__(self, title = '', title_color = 'white', header = False, header_alignment = 'center', header_color = 'white', data = [[' ']], alignment = 'left', style = 0, shadow = False, shadow_color = 'white', indent = 0, text_color = 'white', borders_color = 'white'):
+	def __init__(self, title = '', title_color = 'white', header = False, header_alignment = 'center', header_color = 'white', data = [[' ']], alignment = 'left', style = 0, shadow_color = None, indent = 0, text_color = 'white', borders_color = 'white', header_bg_color = None):
 		'''
 		Вывод данных в табличном виде.
 
@@ -66,11 +36,14 @@ class Mtable:
 		Заголовок:
 		header = True - выделять первую строку ЗАГЛАВНЫМИ БУКВАМИ
 
-		Выравнивание заголовков столбцов:
+		Выравнивание текста заголовков столбцов столбцов:
 		header_alignment = 'left/center/right' - по умолчанию left. Выравнивание применяется только если параметра header = True
 		
-		Цвет заголовков таблицы:
+		Цвет текста заголовков столбцов таблицы:
 		header_color = 'white'
+
+		Цвет фона заголовков столбцов таблицы:
+		header_bg_color = 'red'
 
 		Данные:
 		data = [[1,'Ivan','Petrov],[2,'Max','Chubin']]
@@ -83,20 +56,22 @@ class Mtable:
 		Цвет текста в таблице:
 		text_color = 'white'
 
-		Table borders styles:
-		* 	0 - single line borders and dividers
-		*	1 - double line borders and dividers
-		*	2 - double line external borders and single line dividers
-		*	3 - no external borders, only single line dividers and corners
-		*	4 - single line external borders with rows and columns marker
-		*	5 - single line external borders
-		*	6 - double line external borders with rows and columns marker
-		*	7 - double line external borders
+		Стили границ таблицы:
+		* 	0 - одинарная граница и разделители/singleline borders and dividers
+		*	1 - двойная граница и разделители/double line borders and dividers
+		*	2 - двойная гнраница и одинарные разделители/double line external borders and single line dividers
+		*	3 - без внешней границы, только одинарный разделитель и углы/no external borders, only single line dividers and corners
+		*	4 - одинарны граница с маркерами столбцов и строк/single line external borders with rows and columns marker
+		*	5 - только одинарная граница/single line external borders
+		*	6 - двойная граница с маркерами столбцов и строк/double line external borders with rows and columns marker
+		*	7 - только двойная внешняя граница/double line external borders
 		*	8 - ASCII style 1
 		*	9 - ASCII style 2
-		*	10 - formated data with divider crossroads 
-		*	11 - formated data with corners
-		*	12 - formated data without divider lines
+		*	10 - отформатированные данные с перекрестиями/formated data with divider crossroads 
+		*	11 - отформатированные данные с уголками/formated data with corners
+		*	12 - отформатированные данные без разделительных линий/formated data without divider lines
+		*	13 - одинарная внешняя рамка/external sinle line border
+		*	14 - двойная внешняя рамка/external double line border
 
 		Цвет тиний таблицы:
 		borders_color = 'white'
@@ -124,38 +99,142 @@ class Mtable:
 		tbl1 = Table(title = 'Table title', header = True, header_alignment = 'center', data = [[1,'Ivan','Petrov],[2,'Max','Chubin']], alignment = 'left', style = 2, indent = 3)
 		tbl.show()
 		'''
-		style = int(style)
-		if style > len(self.styles):
-			style = 0
+
+		# символы для отрисовки узлов таблицы
+		self.styles = [	
+					['┌','─','┐','└','┘','│','┬','┴','┼','├','┤','│','─'],	# 0
+					['╔','═','╗','╚','╝','║','╦','╩','╬','╠','╣','║','═'],	# 1
+					['╔','═','╗','╚','╝','║','╤','╧','┼','╟','╢','│','─'],	# 2
+					['┌',' ','┐','└','┘',' ',' ',' ','┼',' ',' ','│','─'],	# 3
+					['┌','─','┐','└','┘','│','┬','┴',' ','├','┤',' ',' '],	# 4
+					['┌','─','┐','└','┘','│','─','─',' ','│','│',' ',' '],	# 5
+					['╔','═','╗','╚','╝','║','╤','╧',' ','╟','╢',' ',' '],	# 6
+					['╔','═','╗','╚','╝','║','═','═',' ','║','║',' ',' '],	# 7
+					['+','-','+','+','+','|','-','-','+','|','|','|','-'],	# 8
+					['+','=','+','+','+','|','=','=','+','|','|','|','='],	# 9
+					['┌',' ','┐','└','┘',' ','┬','┴','┼','├','┤',' ',' '],	# 10
+					['┌',' ','┐','└','┘',' ',' ',' ',' ',' ',' ',' ',' '],	# 11
+					[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],	# 12
+					['┌','─','┐','└','┘','│',' ',' ',' ','│','│','│',' '],	# 13
+					['╔','═','╗','╚','╝','║',' ',' ',' ','║','║','║',' ']	# 14
+				]
+		
+
+		self.text_colors = {
+			'black': 		30,
+			'red': 			31,
+			'green': 		32,
+			'yellow':		33,
+			'blue': 		34,
+			'violet': 		35,
+			'turquoise': 	36,
+			'white': 		37,
+		}
+
+		# символы для отрисовки тени
+		self.shadow_symbol = ['█','▀']
+
+		try:
+			self.style = int(style)
+		except Exception as ex:
+			self.style = 0
+		if style > len(self.styles) or style < 0:
+			self.style = 0
 		self.symbols = self.styles[style]
-		self.title = title
-		self.header = header
-		self.shadow = shadow
+		self.title = title.strip()
+		self.header = bool(header)
+		# self.shadow = bool(shadow)
 		self.alignment = alignment.lower()
 		self.header_alignment = header_alignment.lower()
-		self.indent = int(indent)
-		self.text_color = text_color.lower()
-		self.borders_color = borders_color.lower()
-		self.title_color = title_color.lower()
-		self.header_color = header_color.lower()
-		self.shadow_color = shadow_color.lower()
-		self.tofile = False
+		try:
+			self.indent = int(indent)
+		except Exception as ex:
+			self.indent = 0
+		if indent > 50:
+			indent = 50
+		self.text_color 	= self.__checkType(text_color)
+		self.borders_color 	= self.__checkType(borders_color)
+		self.title_color 	= self.__checkType(title_color)
+		self.header_color 	= self.__checkType(header_color)
+		self.shadow_color 	= self.__checkType(shadow_color)
 
-		if type(data) == str:
+
+		if header_bg_color:
+			self.bg_color 	= self.__checkType(header_bg_color) + 10 # цвет фона отличается на 10 от цвета шрифта
+		else:
+			self.bg_color 	= None
+
+		self.tofile = False
+		if isinstance(data, dict): # если передали словарь, то отформатировать в правильный вид
+			data = [list(data.keys()), list(data.values())]
+		elif isinstance(data, str): # если передали строку, то схавать
 			data = [[data]]
-		elif type(data[0]) == str:
+		elif isinstance(data[0], str): # если передали список, то отформатировать в правильный вид
 			data = [data]
+		elif isinstance(data, tuple): # если передали кортеж, то отформатировать в правильный вид
+			data = [list(data)]
 		self.data = data
 		self.rows = len(data)
-		self.columns = len(data[0])
+		# найдем MAX кол-во столбцов перебирая строки 
+		self.columns = 0
+		for item in data:
+			if self.columns < len(item):
+				self.columns = len(item)
 		self.columns_max = self.__max_len(self.data)
 		self.table_width = self.__sum(self.columns_max) + 2 + self.columns
+	
+	def __checkType(self, param):
+		'''Если цвет был передан строкой то приводим к нижнему регистру, а если числом, то возвращаем это число. В случае неправильного типа задаем цвет  WHITE = 37'''
+		if isinstance(param, str):
+			return param.lower()
+		elif isinstance(param, int):
+			return param
+		else:
+			return 37
+		
+
+	@property
+	def style_counter(self):
+		'''максимально доступное количество стилей'''
+		return len(self.styles)
+
+	def color(self, text, color = 'white', bg = None, bold = False, cont = False):
+		if self.tofile:
+			out = text
+		else:
+			if isinstance(color, str):
+				col = self.text_colors.get(color, 37)
+			elif isinstance(color, int):
+				col = color
+			else:
+				col = 37
+
+			if isinstance(bg, str):
+				bg_col = self.text_colors.get(bg, -1)
+				if bg_col != -1 :
+					bg_col = f'\033[{bg_col}m'
+			elif isinstance(bg, int):
+				bg_col = f'\033[{bg}m'
+			else:
+				bg_col = ''
+
+			BOLD = ""
+			if bold:
+				BOLD = "\033[1m"
+			if cont:
+				fin = ""
+			else:
+				fin = "\033[0m"
+			out = f"{BOLD}\033[{col}m{bg_col}{text}{fin}"
+		return out
+
 
 	def show(self):
 		'''Выводит таблицу с цветовой разметкой в консоль'''
 		table = self.__draw()
 		for line in table:
 			print(line)
+		return table
 
 	def draw(self):
 		'''Возвращает таблицу как текст без консольной цветной разметки'''
@@ -172,7 +251,10 @@ class Mtable:
 		for column_number in range(self.columns):
 			max_len = 0
 			for line_number in range(self.rows):
-				item_len = len(str(self.data[line_number][column_number])) + self.indent*2
+				try:
+					item_len = len(str(self.data[line_number][column_number])) + self.indent*2
+				except:
+					item_len = 0
 				if max_len < item_len: 
 					max_len = item_len
 			columns_max.append(max_len)    
@@ -194,7 +276,7 @@ class Mtable:
 		top_line = self.color(self.symbols[0], self.borders_color, cont = 1)
 		for t in range(len(self.columns_max) ):
 			top_line = top_line + self.symbols[1]*self.columns_max[t]
-			if t < len(self.columns_max)-1: 
+			if t < len(self.columns_max) - 1: 
 				top_line += self.symbols[6]
 		top_line += self.color(self.symbols[2],self.borders_color)
 
@@ -205,7 +287,7 @@ class Mtable:
 			if t < len(self.columns_max)-1:
 				bottom_line += self.symbols[7]
 		bottom_line += self.color(self.symbols[4], self.borders_color)
-		if self.shadow:
+		if self.shadow_color:
 				bottom_line += self.color(self.shadow_symbol[0], self.shadow_color)
 
 		# создание средней линии
@@ -215,7 +297,7 @@ class Mtable:
 			if t < len(self.columns_max)-1:
 				middle_line += self.symbols[8]
 		middle_line += self.color(self.symbols[10],self.borders_color)
-		if self.shadow:
+		if self.shadow_color:
 				middle_line += self.color(self.shadow_symbol[0], self.shadow_color)
 
 		table.append(top_line)
@@ -225,31 +307,39 @@ class Mtable:
 			line = ''
 			line += self.color(self.symbols[5], self.borders_color)
 			for column_number in range(self.columns):
-				cell = ' ' * self.indent + str(self.data[line_number][column_number]) + ' ' * self.indent
+				bg_color = None
+				bold = False
+				try:
+					elem = str(self.data[line_number][column_number]).replace('\n', '').strip()
+				except:
+					elem = ''
+				cell = ' ' * self.indent + elem + ' ' * self.indent
 				if line_number == 0 and self.header:
 					# для первой строки таблицы  используем параметр выравнивания как для заголовка
 					cell = cell.upper()
 					aligment = self.header_alignment
 					color = self.header_color
+					bg_color = self.bg_color
+					bold = True
 				else:
 					# для остальных строк используем параметр выравнивания для содержимого
 					aligment = self.alignment
 					color = self.text_color
 				if aligment == 'right':
-					line += self.color(cell.rjust(self.columns_max[column_number]), color)
+					line += self.color(cell.rjust(self.columns_max[column_number]), color, bg = bg_color, bold=bold)
 				elif aligment == 'center':
-					line += self.color(cell.center(self.columns_max[column_number]), color)
+					line += self.color(cell.center(self.columns_max[column_number]), color, bg = bg_color, bold=bold)
 				else:
-					line += self.color(cell.ljust(self.columns_max[column_number]), color)
+					line += self.color(cell.ljust(self.columns_max[column_number]), color, bg = bg_color, bold=bold)
 				if column_number < len(self.columns_max) - 1:
 					line += self.color(self.symbols[11], self.borders_color)
 			line += self.color(self.symbols[5], self.borders_color)
-			if self.shadow:
+			if self.shadow_color:
 				line += self.color(self.shadow_symbol[0], self.shadow_color)
 			table.append(line)
 			if line_number < self.rows - 1:
 				table.append(middle_line)
 		table.append(bottom_line)
-		if self.shadow:
+		if self.shadow_color:
 			table.append('  ' + self.color(self.shadow_symbol[1] * (self.table_width - 2), self.shadow_color))
 		return table
